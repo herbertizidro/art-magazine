599eb64f807dcd36a7e8aaae6906499ac6e18c70
Herbert Izidro <herbertsouza80@yahoo.com>
2020-12-12 00:02:45 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(".menu-desktop li a").click(function(){
+    $(".menu-desktop li a").removeClass("active")
+    $(this).addClass("active");
+})
+
+
+$(".menu-mobile li a").click(function(){
+    $(".menu-mobile li a").removeClass("active")
+    $(this).addClass("active");
+})

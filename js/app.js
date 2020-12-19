dd6e5429e91822f948ad6176ccbf6f349bd93504
Herbert Izidro <herbertsouza80@yahoo.com>
2020-12-19 17:18:25 -0300

    document.ready adicionado

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
-$(".menu-desktop li a").click(function(){
-    $(".menu-desktop li a").removeClass("active")
-    $(this).addClass("active");
-})
+$(document).ready(function(){ /* espera a página carregar */
 
+    /* mantém a página atual selecionada na navbar com um border-bottom - desktop */
+    $(".menu-desktop li a").click(function(){
+        $(".menu-desktop li a").removeClass("active")
+        $(this).addClass("active");
+    })
 
-$(".menu-mobile li a").click(function(){
-    $(".menu-mobile li a").removeClass("active")
-    $(this).addClass("active");
-})
+    /* mantém a página atual selecionada na navbar com um border-bottom - mobile */
+    $(".menu-mobile li a").click(function(){
+        $(".menu-mobile li a").removeClass("active")
+        $(this).addClass("active");
+    })
+
+}

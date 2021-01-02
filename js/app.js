b9537152f078e60d125e482a9a34f1746aa05e2f
Herbert Izidro <herbertsouza80@yahoo.com>
2021-01-02 02:38:49 -0300

    função adicionada

--- js/app.js
+++ js/app.js
@@ -11,5 +11,15 @@ $(document).ready(function(){ /* espera a página carregar */
         $(".menu-mobile li a").removeClass("active")
         $(this).addClass("active");
     })
+    
+    /* função que chama o modal assim que a index é carregada */
+    function callModal(){
+        $(".modal").modal({
+            fadeDuration: 500,
+            clickClose: true
+        })
+    }
+    
+    callModal();
 
 }
